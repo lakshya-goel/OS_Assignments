27472835dd769de1ac7434e47a2cfe1841c2472c
lakshya-goel <92212186+lakshya-goel@users.noreply.github.com>
2022-11-24 12:26:16 +0530

    Update test.c

--- Assignment2/test.c
+++ Assignment2/test.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 
-#define SYS_kernel_2D_memcpy 448
+#define SYS_kernel_2D_memcpy 451
 int main(){
   float matrix1[4][4] = {{1.2, 2.3, 4.9, 5.5}, {2.4, 6.9, 4.3, 9.8}, {1.1, 1.3, 1.6, 3.2}, {5.2, 6.5, 7.8, 4.0}};
 
