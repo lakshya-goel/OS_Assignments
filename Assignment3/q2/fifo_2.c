4be456f4c2e0f177c9917c2a54419a40c092e2af
lakshya-goel <92212186+lakshya-goel@users.noreply.github.com>
2022-12-21 23:57:53 +0530

    Update fifo_2.c

--- Assignment3/q2/fifo_2.c
+++ Assignment3/q2/fifo_2.c
@@ -7,46 +7,67 @@
 #include <stdlib.h>
 #include <time.h>
 
-void printStrings(char buff[5][4]){
-	printf("Strings read:\n");
-    for (int i = 0; i <= 4; i++)
-    {	
-		for(int j=0;j<4;j++){
-			printf("%c",buff[i][j]);
-		}
-        printf("\n");
-        
+enum
+{
+    NS_PER_SECOND = 1000000000
+};
+
+struct timespec start,finish,gap;
+
+void sub_timespec(struct timespec t1, struct timespec t2, struct timespec *td)
+{
+    td->tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    td->tv_sec = t2.tv_sec - t1.tv_sec;
+    if (td->tv_sec > 0 && td->tv_nsec < 0)
+    {
+        td->tv_nsec += NS_PER_SECOND;
+        td->tv_sec--;
+    }
+    else if (td->tv_sec < 0 && td->tv_nsec > 0)
+    {
+        td->tv_nsec -= NS_PER_SECOND;
+        td->tv_sec++;
     }
 }
 
-int main()
-{   
-    char read_buff[5][4];
-	int ret;
-    int fd, fd1;
+void printStrings(char buff[5][5]){
+	printf("Strings read from P1:\n");
+    for(int i=0;i<5;i++){
+		    for (int j=0; j<4; j++) {
+                printf("%c", buff[i][j]);
+            }
+            printf("\n");
+	    } 
+}
+
+int main() {   
+    char read_buff[5][5];
+    int fd;
     int index=0;
-    if((fd = open("myfifo1",O_RDONLY))<0){
-			perror("fifo1");
-			exit(1);
-	}
-    if((fd1 = open("myfifo2",O_WRONLY))<0){
-			perror("fifo2");
-			exit(0);
-		}
-    while(index<50){
-		if((ret = read(fd, read_buff,sizeof(read_buff)))<0){
-			perror("error reading");
-			exit(1);
-		}
-		index = read_buff[4][3];
+	clock_gettime(CLOCK_REALTIME, &start);
+    while(index<10) {
+        fd = open("myfifo1",O_RDONLY);
+        if (!fd) {
+            perror("NOT OPEN\n");
+            exit(1);
+        }
+		read(fd, &read_buff, sizeof(read_buff));
 		printStrings(read_buff);
-		close(fd);
-		if(ret = write(fd, read_buff[4], sizeof(read_buff[4])<0)){
-			perror("error writing");
-			exit(0);
-		}
-    }
-    close(fd);
-    close(fd1);
-    return 0;
+        close(fd);
+        char buffer[5];
+        for (int i=0; i<5; i++) {
+            buffer[i] = read_buff[4][i];
+        }
+        fd = open("myfifo1", O_WRONLY);
+        if (!fd) {
+            perror("NOT OPEN\n");
+            exit(1);
+        }
+        write(fd, buffer, sizeof(buffer));
+        close(fd);
+        index++;
+	}
+	clock_gettime(CLOCK_REALTIME, &finish);
+    sub_timespec(start, finish, &gap);
+    printf("Time taken: FIFO-1 - %ld.%ld\n", gap.tv_sec, gap.tv_nsec);
 }
