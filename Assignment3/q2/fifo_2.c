27b8416a5b9e6e13cf0aa945a25a024c4c196f7a
lakshya-goel <92212186+lakshya-goel@users.noreply.github.com>
2022-12-22 00:04:32 +0530

    Update fifo_2.c

--- Assignment3/q2/fifo_2.c
+++ Assignment3/q2/fifo_2.c
@@ -69,5 +69,5 @@ int main() {
 	}
 	clock_gettime(CLOCK_REALTIME, &finish);
     sub_timespec(start, finish, &gap);
-    printf("Time taken: FIFO-1 - %ld.%ld\n", gap.tv_sec, gap.tv_nsec);
+    printf("Time taken: FIFO-2 - %ld.%ld\n", gap.tv_sec, gap.tv_nsec);
 }
