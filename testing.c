b72117900105d42fc0bb4d0d0469e444db734b4f
lakshya-goel <92212186+lakshya-goel@users.noreply.github.com>
2022-12-22 10:40:37 +0530

    Update testing.c

--- testing.c
+++ testing.c
@@ -1,34 +1,51 @@
-#include <linux/unistd.h>
+#include <linux/types.h>
+#include <linux/sched.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/time.h>
+#include <linux/cred.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/uidgid.h>
 #include <linux/syscalls.h>
+#include <linux/unistd.h>
 #include <linux/moduleparam.h>
 
+
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Dhruv Sood");
+MODULE_AUTHOR("Paras Dhiman");
+MODULE_DESCRIPTION( "The PID of the process whose task_struct details we want to retrieve and print");
 
-int pid = 0; 
-module_param(pid, int, S_IRUGO|S_IWUSR);
+static int pid = -1;
+struct task_struct *task;
+module_param(pid,int ,S_IRUGO);
+MODULE_PARM_DESC(pid,"The pid of task command line")
 
-static int __init Iniitializing(void) {
-    struct task_struct *task;
-    task = pid_task(find_vpid(pid), PIDTYPE_PID);
-    if (task == NULL) {
-        printk(KERN_ERR "Invalid PID\n");
-        return -EINVAL;
-    }
-    else{
-        printk("PID %d\n", task->pid);
-        printk("User ID %d\n", task->cred->uid.val);
-        printk("PGID %d\n", task->group_leader->pid);
-        printk("Command Path %s\n", task->comm);
-    }
-    return 0;
+static int __init paras_init(void)
+{
+       	task = pid_task(find_vpid(pid),PIDTYPE_PID);
+	
+	printk("Looking up task_struct details for PID %d\n", pid);
+	if (task) {
+		printk("%d :pid\n",task->pid);
+		printk("%d :user_id\n",task->cred->uid);
+		printk("%d :pgid\n",current->cred->egid);
+		printk("%s : command path\n",task->comm);
+	}
+	else
+	{
+		printk("NO VALID FUNCTION\n")
+	}
+
+	return 0;
 }
 
-static void __exit Exiting(void) {
-    printk("Exiting");
+static void __exit paras_exit(void) {
+	printk("Exiting task_struct_details module\n");
+	// No cleanup needed
 }
 
-module_init(Iniitializing);
-module_exit(Exiting);
+module_init(paras_init);
+module_exit(paras_exit);
+
+
