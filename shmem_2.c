47e3f6a2deaf6078eb0e6b05cadf3f5ad61bb861
lakshya-goel <92212186+lakshya-goel@users.noreply.github.com>
2022-12-22 12:38:33 +0530

    Create shmem_2.c

---
+++ shmem_2.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+
+#define TICK struct timespec
+#define SZE 1024
+
+#define PATH "ipc_shm_p1.c"
+
+int err;
+
+char *shared_init(int *key, int *shmid)
+{
+    *key = ftok(PATH, 'E');
+    if (*key < 0)
+        perror("key error");
+    *shmid = shmget(*key, SZE, IPC_CREAT | 0644);
+    if (*shmid < 0)
+        perror("shmid error");
+    char *ret = shmat(*shmid, NULL, 0);
+    if (ret == (char *)-1)
+        perror("shmat error");
+    return ret;
+}
+
+int main(int argc, char const *argv[])
+{
+    int key, shmid;
+    char *shm = shared_init(&key, &shmid);
+    int strt = 0;
+    while (1)
+    {
+        int idx;
+        char *s = shm;
+        for (int i = strt; i < (strt + 5); i++)
+        {
+            idx = *s++ - 40;
+            printf("%d ", idx);
+            printf("%s\n", s);
+            s += 8;
+        }
+        *shm = (idx + 40);
+        if (strt == 45)
+            break;
+        while (*shm != (idx + 41))
+            ;
+        strt += 5;
+    }
+    shmdt(shm);
+    shmctl(shmid,IPC_RMID,NULL);
+    return 0;
+}
